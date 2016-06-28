ae3e29284834d2cb829fa0a2157266fc81f4dfe3
Helltar <helltar.live@gmail.com>
2016-06-28 14:05:29 +0300

    ProjectConfig: ini4j -> java.util.Properties

--- app/src/main/java/com/github/helltar/anpaside/project/ProjectConfig.java
+++ app/src/main/java/com/github/helltar/anpaside/project/ProjectConfig.java
@@ -1,101 +1,92 @@
 package com.github.helltar.anpaside.project;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import org.ini4j.Wini;
+import java.util.Properties;
 
 public class ProjectConfig {
 
-    private Wini ini;
+    private Properties p = new Properties();
 
-    private final String SECTION_MAIN = "MAIN";
-    private final String SECTION_MANIFEST = "MANIFEST";
-    private final String SECTION_VERSIONS = "VERSIONS";
-
-    public ProjectConfig(String filename) throws IOException {
-        File file = new File(filename);
-
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-
-        ini = new Wini(file);
+    public void open(String filename) throws IOException {
+        p.load(new FileInputStream(filename));
     }
 
-    public void save() throws IOException {
-        ini.store();
+    public void save(String filename) throws IOException {
+        p.store(new FileOutputStream(filename), null);
     }
 
-    public void setMainModuleName(String val) {
-        ini.put(SECTION_MAIN, "MainModule", val);
+    public void setMainModuleName(String mainModule) {
+        p.setProperty("MainModule", mainModule);
     }
 
     public String getMainModuleName() {
-        return ini.get(SECTION_MAIN, "MainModule");
+        return p.getProperty("MainModule", "");
     }
 
-    public void setMathType(int val) {
-        ini.put(SECTION_MAIN, "MathType", val);
+    public void setMathType(int mathType) {
+        p.setProperty("MathType", Integer.toString(mathType));
     }
 
     public int getMathType() {
-        return ini.get(SECTION_MAIN, "MathType", int.class);
+        return Integer.parseInt(p.getProperty("MathType", "0"));
     }
 
-    public void setCanvasType(int val) {
-        ini.put(SECTION_MAIN, "CanvasType", val);
+    public void setCanvasType(int canvasType) {
+        p.setProperty("CanvasType", Integer.toString(canvasType));
     }
 
     public int getCanvasType() {
-        return ini.get(SECTION_MAIN, "CanvasType", int.class);
+        return Integer.parseInt(p.getProperty("CanvasType", "1"));
     }
 
-    public void setMidletName(String val) {
-        ini.put(SECTION_MANIFEST, "Name", val);
+    public void setMidletName(String midletName) {
+        p.setProperty("Name", midletName);
     }
 
     public String getMidletName() {
-        return ini.get(SECTION_MANIFEST, "Name");
+        return p.getProperty("Name", "app");
     }
 
-    public void setMidletVendor(String val) {
-        ini.put(SECTION_MANIFEST, "Vendor", val);
+    public void setMidletVendor(String midletVendor) {
+        p.setProperty("Vendor", midletVendor);
     }
 
     public String getMidletVendor() {
-        return ini.get(SECTION_MANIFEST, "Vendor");
+        return p.getProperty("Vendor", "vendor");
     }
 
-    public void setMidletIcon(String val) {
-        ini.put(SECTION_MANIFEST, "Icon", val);
+    public void setMidletIcon(String midletIcon) {
+        p.setProperty("Icon", midletIcon);
     }
 
     public String getMidletIcon() {
-        return ini.get(SECTION_MANIFEST, "Icon");
+        return p.getProperty("Icon", "/icon.png");
     }
 
-    public void setVersMajor(int val) {
-        ini.put(SECTION_VERSIONS, "Major", val);
+    public void setVersMajor(int versMajor) {
+        p.setProperty("Major", Integer.toString(versMajor));
     }
 
     public int getVersMajor() {
-        return ini.get(SECTION_VERSIONS, "Major", int.class);
+        return Integer.parseInt(p.getProperty("Major", "1"));
     }
 
-    public void setVersMinor(int val) {
-        ini.put(SECTION_VERSIONS, "Minor", val);
+    public void setVersMinor(int versMinor) {
+        p.setProperty("Minor", Integer.toString(versMinor));
     }
 
     public int getVersMinor() {
-        return ini.get(SECTION_VERSIONS, "Minor", int.class);
+        return Integer.parseInt(p.getProperty("Minor", "0"));
     }
 
-    public void setVersBuild(int val) {
-        ini.put(SECTION_VERSIONS, "Build", val);
+    public void setVersBuild(int versBuild) {
+        p.setProperty("Build", Integer.toString(versBuild));
     }
 
     public int getVersBuild() {
-        return ini.get(SECTION_VERSIONS, "Build", int.class);
+        return Integer.parseInt(p.getProperty("Build", "0"));
     }
 }
 
