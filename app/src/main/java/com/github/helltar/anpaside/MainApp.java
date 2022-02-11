98ba89f4e6e95a584bcf60f82e69a72c00b5925a
Helltar <mail@helltar.com>
2022-02-12 01:37:00 +0200

    cosmetic

--- app/src/main/java/com/github/helltar/anpaside/MainApp.java
+++ app/src/main/java/com/github/helltar/anpaside/MainApp.java
@@ -11,6 +11,7 @@ public class MainApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         context = this;
 
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
