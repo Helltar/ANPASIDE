ff408fae0ac6ea49fb980124e33d2083468c3944
Helltar <mail@helltar.com>
2022-07-02 19:58:44 +0300

    cosmetic

--- app/src/main/java/com/github/helltar/anpaside/activities/MainActivity.java
+++ app/src/main/java/com/github/helltar/anpaside/activities/MainActivity.java
@@ -66,10 +66,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         activity = this;
 
