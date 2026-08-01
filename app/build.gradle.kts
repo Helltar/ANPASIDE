dfeaa2d40e541d6f67f99772d80e2893f24aec0d
Helltar <iam@helltar.com>
2026-08-01 22:55:46 +0300

    release 2.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,8 +75,8 @@ android {
         applicationId = "com.github.helltar.anpaside"
         minSdk = 28
         targetSdk = 36
-        versionCode = 38
-        versionName = "2.3.0"
+        versionCode = 39
+        versionName = "2.3.1"
     }
 
     buildTypes {
